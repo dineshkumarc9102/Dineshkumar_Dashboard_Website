1009b8503964af12170e1c7d5c457957629e9445
Dinesh Kumar <125630400+dineshkumarc9102@users.noreply.github.com>
2026-08-08 11:28:08 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,11 +7,23 @@ import PO from "./Components/Po";
 import Stock from "./Components/Stock";
 import Login from "./Components/Login";
 import PrivateRoute from "./Components/PrivateRoute";
+import BasicCalc from "./Components/Calculators/BasicCalc";
+import EMICalc from "./Components/Calculators/EMICalc";
+import FDCalc from "./Components/Calculators/FDCalc";
+import InflationCalc from "./Components/Calculators/InflationCalc";
+import ITCalc from "./Components/Calculators/ITCalc";
+import LumpsumCalc from "./Components/Calculators/LumpsumCalc";
+import PLCalc from "./Components/Calculators/P&LCalc";
+import PPFCalc from "./Components/Calculators/PPFCalc";
+import RDCalc from "./Components/Calculators/RDCalc";
+import SIPCalc from "./Components/Calculators/SIPCalc";
+import StepUpCalc from "./Components/Calculators/StepUpCalc";
+import SWPCalc from "./Components/Calculators/SWPCalc";
 
 export default function App() {
   return (
     <Routes>
-      
+
       {/* ✅ PUBLIC ROUTE */}
       <Route path="/login" element={<Login />} />
 
@@ -28,6 +40,20 @@ export default function App() {
         <Route path="salary" element={<Salary />} />
         <Route path="po" element={<PO />} />
         <Route path="stock" element={<Stock />} />
+        <Route path="/calculator">
+          <Route path="basic" element={<BasicCalc />} />
+          <Route path="emi" element={<EMICalc />} />
+          <Route path="fd" element={<FDCalc />} />
+          <Route path="inflation" element={<InflationCalc />} />
+          <Route path="it" element={<ITCalc />} />
+          <Route path="lump" element={<LumpsumCalc />} />
+          <Route path="pl" element={<PLCalc />} />
+          <Route path="ppf" element={<PPFCalc />} />
+          <Route path="rd" element={<RDCalc />} />
+          <Route path="sip" element={<SIPCalc />} />
+          <Route path="stepup" element={<StepUpCalc />} />
+          <Route path="swp" element={<SWPCalc />} />
+        </Route>
       </Route>
 
     </Routes>
